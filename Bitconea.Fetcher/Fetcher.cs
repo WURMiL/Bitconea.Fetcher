ebe865b295c2bb36e8c7e7f9c648d99756dfd569
WURMiL <wurmi@dbs.sk>
2022-10-04 22:40:54 +0200

    #1.1.0 added DI. chenged class and method signatures

--- Bitconea.Fetcher/Fetcher.cs
+++ Bitconea.Fetcher/Fetcher.cs
@@ -7,81 +7,97 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 
 namespace Bitconea.Fetcher
 {
     public class Fetcher
     {
+        private readonly ILogger<Fetcher> _logger;
         private static readonly ConcurrentDictionary<FetcherJob, object> ActiveJobs = new();
         private static readonly SemaphoreSlim Semaphore;
 
-        private readonly HttpClient _httpClient;
-        private readonly FetcherJob _fetcherJob;
+        //private readonly HttpClient _httpClient;
+        //private readonly FetcherJob _fetcherJob;
 
         static Fetcher()
         {
             Semaphore = new(4);
         }
 
-        public Fetcher(FetcherJob fetcherJob)
+        public Fetcher(ILogger<Fetcher> logger)
         {
-            var timeout = _fetcherJob.Timeout != TimeSpan.Zero ? _fetcherJob.Timeout : TimeSpan.FromSeconds(15);
-
-            _httpClient = new HttpClient(new HttpClientHandler { AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate }) { Timeout = timeout };
-            _fetcherJob = fetcherJob;
+            _logger = logger;
         }
 
-        public Fetcher(HttpClient httpClient, FetcherJob fetcherJob)
-        {
-            _httpClient = httpClient;
-            _fetcherJob = fetcherJob;
-        }
+        //public Fetcher(FetcherJob fetcherJob)
+        //{
+        //    var timeout = _fetcherJob.Timeout != TimeSpan.Zero ? _fetcherJob.Timeout : TimeSpan.FromSeconds(15);
 
-        public async Task<FetcherJob> Fetch()
+        //    _httpClient = new HttpClient(new HttpClientHandler { AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate }) { Timeout = timeout };
+        //    _fetcherJob = fetcherJob;
+        //}
+
+        //public Fetcher(HttpClient httpClient, FetcherJob fetcherJob)
+        //{
+        //    _httpClient = httpClient;
+        //    _fetcherJob = fetcherJob;
+        //}
+
+        public async Task Fetch(FetcherJob fetcherJob, HttpClient httpClient = null)
         {
+            if (httpClient == null)
+            {
+                var timeout = fetcherJob.Timeout != TimeSpan.Zero ? fetcherJob.Timeout : TimeSpan.FromSeconds(15);
+                httpClient = new HttpClient(new HttpClientHandler { AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate }) { Timeout = timeout };
+            }
+
             await Semaphore.WaitAsync();
 
-            if (_fetcherJob.WaitForFinishPendingRequestFromSameHost)
+            if (fetcherJob.WaitForFinishPendingRequestFromSameHost)
             {
-                while (ActiveJobs.Keys.Any(x => x.Uri.Host == _fetcherJob.Uri.Host))
+                while (ActiveJobs.Keys.Any(x => x.Uri.Host == fetcherJob.Uri.Host))
                 {
                     await Task.Delay(100);
                 }
-                ActiveJobs.TryAdd(_fetcherJob, null);
+                ActiveJobs.TryAdd(fetcherJob, null);
             }
 
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
             //Console.WriteLine($"{DateTime.Now.ToLongTimeString()}, {DateTime.Now.Millisecond}: Start fetching from {_fetcherJob.Uri.OriginalString}");
+            _logger.LogInformation("Start fetching from {Url}", fetcherJob.Uri.OriginalString);
             var jobResponse = new FetcherJobResponse();
 
-            _httpClient.DefaultRequestHeaders.Clear();
-            _httpClient.DefaultRequestHeaders.Authorization = _fetcherJob.AuthenticationHeaderValue;
+            httpClient.DefaultRequestHeaders.Clear();
+            httpClient.DefaultRequestHeaders.Authorization = fetcherJob.AuthenticationHeaderValue;
 
             try
             {
                 HttpResponseMessage message;
 
-                foreach (var customHeader in _fetcherJob.CustomHeaders)
-                    _httpClient.DefaultRequestHeaders.Add(customHeader.Key, customHeader.Value);
+                foreach (var customHeader in fetcherJob.CustomHeaders)
+                {
+                    httpClient.DefaultRequestHeaders.Add(customHeader.Key, customHeader.Value);
+                }
 
-                if (_fetcherJob.HttpMethod == HttpMethod.Get)
+                if (fetcherJob.HttpMethod == HttpMethod.Get)
                 {
-                    message = await _httpClient.GetAsync(_fetcherJob.Uri);
+                    message = await httpClient.GetAsync(fetcherJob.Uri);
                 }
-                else if (_fetcherJob.HttpMethod == HttpMethod.Put)
+                else if (fetcherJob.HttpMethod == HttpMethod.Put)
                 {
-                    _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                    message = await _httpClient.PutAsync(_fetcherJob.Uri, _fetcherJob.HttpContent);
+                    httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    message = await httpClient.PutAsync(fetcherJob.Uri, fetcherJob.HttpContent);
                 }
-                else if (_fetcherJob.HttpMethod == HttpMethod.Post)
+                else if (fetcherJob.HttpMethod == HttpMethod.Post)
                 {
-                    _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                    message = await _httpClient.PostAsync(_fetcherJob.Uri, _fetcherJob.HttpContent);
+                    httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    message = await httpClient.PostAsync(fetcherJob.Uri, fetcherJob.HttpContent);
                 }
                 else
                 {
-                    throw new Exception($"Unsupported HTTP method '{_fetcherJob.HttpMethod}'");
+                    throw new Exception($"Unsupported HTTP method '{fetcherJob.HttpMethod}'");
                 }
 
                 jobResponse.HttpResponseMessage = message;
@@ -95,18 +111,19 @@ namespace Bitconea.Fetcher
                 {
                     if (string.IsNullOrEmpty(jobResponse.RawResponse))
                     {
-                        jobResponse.Exception = new Exception($"Empty response.");
+                        jobResponse.Exception = new Exception("Empty response.");
                         jobResponse.IsSuccessfull = false;
                     }
-                    else if (_fetcherJob.ExpectJson && jobResponse.JToken == null)
+                    else if (fetcherJob.ExpectJson && jobResponse.JToken == null)
                     {
-                        jobResponse.Exception = new Exception($"no JSON response");
+                        jobResponse.Exception = new Exception("no JSON response");
                         jobResponse.IsSuccessfull = false;
                     }
                     else
                     {
                         //jobResponse.IsJson = message.Content.Headers.ContentType?.MediaType == "application/json" && jobResponse.JToken != null;
                         //Console.WriteLine($"{DateTime.Now.ToLongTimeString()}, {DateTime.Now.Millisecond}: SUCCESS response from from {_fetcherJob.Uri.AbsoluteUri}");
+                        _logger.LogInformation("Success response from {Url} with {Duration}ms", fetcherJob.Uri.OriginalString, stopwatch.ElapsedMilliseconds);
                         jobResponse.IsSuccessfull = true;
                     }
                 }
@@ -121,19 +138,18 @@ namespace Bitconea.Fetcher
                 jobResponse.Exception = ex.InnerException?.InnerException ?? ex.InnerException ?? ex;
                 jobResponse.IsSuccessfull = false;
                 //Console.WriteLine($"{DateTime.Now.ToLongTimeString()}, {DateTime.Now.Millisecond}: [{_fetcherJob.Uri.AbsoluteUri}] {(ex.InnerException?.InnerException ?? ex.InnerException ?? ex).Message}");
+                _logger.LogError(jobResponse.Exception, "Fetching data from {Url} failed", fetcherJob.Uri.OriginalString);
             }
             finally
             {
-                ActiveJobs.TryRemove(_fetcherJob, out _);
+                ActiveJobs.TryRemove(fetcherJob, out _);
                 Semaphore.Release();
             }
 
-            _fetcherJob.IsProcessed = true;
+            fetcherJob.IsProcessed = true;
             stopwatch.Stop();
             jobResponse.ElapsedMilliseconds = stopwatch.ElapsedMilliseconds;
-            _fetcherJob.Response = jobResponse;
-
-            return _fetcherJob;
+            fetcherJob.Response = jobResponse;
         }
     }
 }
